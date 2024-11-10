6b55799aa6510e337e5b575854c02f08d1cad779
Nikita <976008@mail.ru>
2024-11-10 20:46:42 +0300

    module2-task2

--- js/functions.js
+++ js/functions.js
@@ -2,10 +2,13 @@ function checkStringLength(textToValidate, maxLength) {
   return textToValidate.length <= maxLength;
 }
 // Строка короче 20 символов
+
 checkStringLength('проверяемая строка', 20); // true
 // Длина строки ровно 18 символов
+
 checkStringLength('проверяемая строка', 18); // true
 // Строка длиннее 10 символов
+
 checkStringLength('проверяемая строка', 10); // false
 
 function isPalindrom(string){
